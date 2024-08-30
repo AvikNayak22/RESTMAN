107930a433cc60ce86d4e2590bf8ce401c37a813
AvikNayak22 <nayakavik19@gmail.com>
2024-08-30 18:03:24 +0530

    started working on tabs component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { Box, MenuItem, Select, TextField, Button } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  component: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  select: {
+    width: 150,
+    height: 40,
+  },
+  textfield: {
+    width: "100%",
+    background: "#f6f6f6",
+  },
+  button: {
+    width: 100,
+    height: 40,
+    marginLeft: [5, "!important"],
+  },
+});
 
 const Form = () => {
-  return <div>Form</div>;
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.component}>
+      <Select label="POST" className={classes.select}>
+        <MenuItem value="POST">POST</MenuItem>
+        <MenuItem value="GET">GET</MenuItem>
+      </Select>
+      <TextField size="small" className={classes.textfield} />
+      <Button className={classes.button} variant="contained">
+        Send
+      </Button>
+    </Box>
+  );
 };
 
 export default Form;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,27 @@
+import { Box } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
 import Form from "./Form";
 import Header from "./Header";
+import SelectTab from "./SelectTab";
+
+const useStyles = makeStyles({
+  component: {
+    width: "60%",
+    margin: "20px auto 0 auto",
+  },
+});
 
 const Home = () => {
+  const classes = useStyles();
+
   return (
     <>
       <Header />
-      <Form />
+      <Box className={classes.component}>
+        <Form />
+        <SelectTab />
+      </Box>
     </>
   );
 };
